e0ee8284f0134a1542f8c60d2591838e8683ec71
Branly Flores <cursosdecomputacion.net@gmail.com>
2020-10-09 23:16:42 -0600

    actualizacion

---
+++ inventario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace inventario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
